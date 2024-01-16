137bf357f91897e6932c82fec3b436b7af23e92d
Norberto Boa <nboa.26@gmail.com>
2024-01-16 14:58:58 +0200

    :tada: feat: accessn the token data on dashboard page

--- web/src/app/(auth)/dashboard/page.tsx
+++ web/src/app/(auth)/dashboard/page.tsx
@@ -1,12 +1,23 @@
 import { cookies } from "next/headers";
+import { JwtPayload, verify } from "jsonwebtoken";
+
+// interface decodedToken extends JwtPayload {
+//   name: string;
+//   email: string;
+// }
 
 export default function Dashboard() {
   const cookieStore = cookies();
   const token = cookieStore.get("auth.token");
+  let decoded;
+  if (token) {
+    decoded = verify(token.value, "Mena") as JwtPayload;
+    console.log(decoded);
+  }
 
   return (
     <div>
-      <p>{token?.value}</p>
+      <p>{decoded?.name}</p>
     </div>
   );
 }
