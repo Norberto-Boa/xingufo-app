da340e4bb7bdfc9f6c0807e4059ba7693bdc636e
Norberto Boa <nboa.26@gmail.com>
2024-01-19 22:45:39 +0200

    fix: fix padding top on layout

--- web/src/components/RightBar.tsx
+++ web/src/components/RightBar.tsx
@@ -2,7 +2,7 @@ import UserTools from "./UserTools";
 
 export default function Rightbar() {
   return (
-    <div className="pt-10 px-8 border-l-2 dark:border-zinc-800 min-w-[280px] max-w-xs">
+    <div className="pt-8 px-8 border-l-2 dark:border-zinc-800 min-w-[280px] max-w-xs">
       <UserTools />
     </div>
   );
