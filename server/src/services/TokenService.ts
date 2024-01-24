ae8c4f81934de992da89de4487019282f94928d2
Norberto Boa <nboa.26@gmail.com>
2024-01-24 23:48:11 +0200

    implement decrypt function

--- server/src/services/TokenService.ts
+++ server/src/services/TokenService.ts
@@ -1,4 +1,10 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { verify } from "jsonwebtoken";
+
+interface decryptedToken extends JwtPayload {
+  email: string;
+  name: string;
+}
 
 export function generateAccessToken(name: string, email: string) {
   const Secret: string = process.env.JWT_SECRET as string;
@@ -6,6 +12,17 @@ export function generateAccessToken(name: string, email: string) {
   return jwt.sign({ name: name, email: email }, Secret, {
     expiresIn: "2h",
   });
+}
+export function decryptToken(token: string): decryptedToken | null {
+  const decryptedToken = verify(token, process.env.JWT_SECRET as string);
+
+  if (typeof decryptToken == "string") {
+    throw new Error("O Token é invalido.");
+  } else if (typeof decryptToken == "object") {
+    return decryptedToken as decryptedToken;
+  }
+  
+  return null;
 }
 
 export function generateRefreshToken(oldRefreshToken: string) {
