bcbda85be0e1cae422050a9b3ca1b0e2a8e357d8
unknown <nboa.26@gmail.com>
2024-03-19 00:54:58 +0200

    feat: pre populate forms for edition

--- web/src/components/UserInformation.tsx
+++ web/src/components/UserInformation.tsx
@@ -1,9 +1,39 @@
+import { baseUrl } from "@/utils/BaseUrl";
 import UserInformationInput from "./UserInformationInput";
+import { CheckIfIsAuthenticatedOnServer } from "@/utils/ServerToken";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
+interface UserDetailsDTO {
+  name: string;
+  email: string;
+  cellphone: string;
+}
 
+async function getUserInformation(): Promise<UserDetailsDTO> {
+  const cookieStore = cookies();
+  const token = cookieStore.get("auth.token");
 
+  if (!token) {
+    redirect("/login");
+  }
 
-export default function UserInformation() {
+  const res = await fetch(`${baseUrl}userinfo`, {
+    headers: {
+      Authorization: "Bearer " + token.value,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data!");
+  }
+
+  return res.json();
+}
+
+export default async function UserInformation() {
+  const userInformation = await getUserInformation();
+
   return (
     <div>
       <div className="mb-3 flex justify-between">
@@ -16,7 +46,27 @@ export default function UserInformation() {
       </div>
 
       <div>
-        <UserInformationInput label="Name" type="text" name="Name" id="Name" />
+        <UserInformationInput
+          label="Name"
+          type="text"
+          name="Name"
+          id="Name"
+          value={userInformation.name}
+        />
+        <UserInformationInput
+          label="Name"
+          type="text"
+          name="Name"
+          id="Name"
+          value={userInformation.email}
+        />
+        <UserInformationInput
+          label="Name"
+          type="text"
+          name="Name"
+          id="Name"
+          value={userInformation.cellphone}
+        />
       </div>
     </div>
   );
--- web/src/components/UserInformationInput.tsx
+++ web/src/components/UserInformationInput.tsx
@@ -5,19 +5,21 @@ import { PencilSimpleLine } from "phosphor-react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  value: string;
 }
 
 export default function UserInformationInput(InputProps: InputProps) {
   const [disabled, setDisabled] = useState(true);
 
   return (
-    <div className="mb-2 flex gap-2 items-center">
+    <div className="mb-4 flex gap-2 items-center">
       <label htmlFor={InputProps.id}>{InputProps.label}</label>
       <input
         name={InputProps.name}
         id={InputProps.id}
         disabled={disabled}
         className="w-3/5 rounded ml-2 py-1 px-2 mr-3 text-slate-900"
+        value={InputProps.value}
       />
 
       <div
